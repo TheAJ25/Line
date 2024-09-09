f82919c9f17720cc6d133ca01b42ac6d298d9b11
Abhijit Shaw <abhijitshaw2501@gmail.com>
2024-09-09 13:37:24 +0530

    Add the ability to draw

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,69 @@
-const { useRef, useEffect } = require("react");
+const { useRef, useEffect, useLayoutEffect } = require("react");
+import { useSelector, useDispatch } from "react-redux";
 
 const Board = () => {
+  const shouldDarw = useRef(false);
   const canvasRef = useRef(null);
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
 
-  useEffect(() => {
+  useEffect(()=>{
+    if(!canvasRef.current) return
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d')
+
+    const changeConfig = () => {
+      context.strokeStyle = color
+      context.lineWidth = size
+    }
+    
+    changeConfig();
+
+  }, [color, size])
+
+
+  // before browser pain
+  useLayoutEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-
+  
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const beginPath = (x, y) => {
+      context.beginPath()
+      context.moveTo(x, y)
+    }
+
+    const drawLine = (x, y) => {
+    context.lineTo(x, y)
+    context.stroke()
+    }
+
+    const handleMouseDown = (e) => {
+      shouldDarw.current = true
+      beginPath(e.clientX, e.clientY)
+    }
+    const handleMouseMove = (e) => {
+      if(!shouldDarw.current) return
+      drawLine(e.clientX, e.clientY)
+    }
+    const handleMouseUp = (e) => {
+      shouldDarw.current = false
+    }
+
+    canvas.addEventListener('mousedown', handleMouseDown)
+    canvas.addEventListener('mousemove', handleMouseMove)
+    canvas.addEventListener('mouseup', handleMouseUp)
+
+    return () => {
+      canvas.removeEventListener('mousedown', handleMouseDown)
+      canvas.removeEventListener('mousemove', handleMouseMove)
+      canvas.removeEventListener('mouseup', handleMouseUp)
+
+    }
+
   }, []);
   return (
   <canvas ref={canvasRef}></canvas>
--- src/components/Menubar/index.js
+++ src/components/Menubar/index.js
@@ -1,4 +1,5 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import cx from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPencilAlt,
@@ -7,23 +8,29 @@ import {
   faRotateRight,
   faFileArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 import { MENU_ITEMS } from "@/contants";
 import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
 
 const Menu = () => {
   const dispatch = useDispatch();
-
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const handleMenuClick = (itemName) => {
     dispatch(menuItemClick(itemName));
   };
 
   return (
     <div className={styles.menuContainer}>
-      <div className={styles.iconwrapper} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+      <div
+        className={cx(styles.iconwrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})}
+        onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
+      >
         <FontAwesomeIcon icon={faPencilAlt} className={styles.icon} />
       </div>
-      <div className={styles.iconwrapper} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+      <div
+        className={cx(styles.iconwrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})}
+        onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
+      >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
       <div className={styles.iconwrapper}>
